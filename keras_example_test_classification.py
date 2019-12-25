d16218905b6052c982c159a951a2fddb6185bb7e
hugo <hjbello.wk@gmail.com>
2019-12-25 23:01:24 +0100

    initial commits and tests

--- keras_example_test_classification.py
+++ keras_example_test_classification.py
@@ -16,6 +16,9 @@ from keras import layers
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.wrappers.scikit_learn import KerasClassifier
+from keras.callbacks import EarlyStopping
+
+early_stopping_monitor = EarlyStopping(patience=3)
 
 import os
 
@@ -31,3 +34,91 @@ for source, filepath in filepath_dict.items():
     df['source'] = source  # Add another column filled with the source name
     df_list.append(df)
 # df_list
+
+df = pd.concat(df_list)
+print(df.iloc[0])
+print(df.tail())
+
+
+# sentences = ['Rashmi likes ice cream', 'Rashmi hates chocolate.']
+# vectorizer = CountVectorizer(min_df=0, lowercase=False)
+# vectorizer.fit(sentences)
+# print(vectorizer.vocabulary_)
+# print(vectorizer.transform(sentences).toarray())
+
+
+from sklearn.model_selection import train_test_split
+
+df_yelp = df[df['source'] == 'yelp']
+
+sentences = df_yelp['sentence'].values
+y = df_yelp['label'].values
+
+sentences_train, sentences_test, y_train, y_test = train_test_split(
+   sentences, y, test_size=0.25, random_state=1000)
+
+from keras.preprocessing.text import Tokenizer
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(sentences_train)
+
+X_train = tokenizer.texts_to_sequences(sentences_train)
+X_test = tokenizer.texts_to_sequences(sentences_test)
+
+vocab_size = len(tokenizer.word_index) + 1  # Adding 1 because of reserved 0 index
+
+print(sentences_train[2])
+print(X_train[2])
+
+
+from keras import layers
+
+#input_dim = X_train.shape[1]  # Number of features
+
+
+from keras.preprocessing.sequence import pad_sequences
+
+maxlen = 100
+
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+print(X_train[0, :])
+
+from keras.models import Sequential
+from keras import layers
+
+embedding_dim = 50
+
+model = Sequential()
+model.add(layers.Embedding(input_dim=vocab_size, 
+                           output_dim=embedding_dim, 
+                           input_length=maxlen))
+model.add(layers.Flatten())
+model.add(layers.Dense(20, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+
+
+history = model.fit(X_train, y_train,
+                    epochs=50,
+                    verbose=True,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+
+
+#test using new data
+sentence_test = ["this is good, yes, so good", "I love it so much", "I believe its ok, great","hate it with all my soul", "not a good experience, real bad", "so bad wrong no no please worst thing ever"]
+xnew = tokenizer.texts_to_sequences(sentence_test)
+xnew = pad_sequences(xnew, padding='post', maxlen=maxlen)
+
+ynew = model.predict_classes(xnew)
+print(ynew)
